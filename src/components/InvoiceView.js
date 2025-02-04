999db348c110d4f01019f37112f970bb8aa18766
chuck at work <charlesnelson03@gmail.com>
2025-02-03 16:55:50 -0800

    pulling dynamic data to invoice view

--- src/components/InvoiceView.js
+++ src/components/InvoiceView.js
@@ -1,77 +1,107 @@
 import React from "react";
-import { useParams } from 'react-router-dom';
-import { Page, Text, View, Document, StyleSheet, PDFViewer } from '@react-pdf/renderer';
+import { useParams } from "react-router-dom";
 import { useGetInvoiceByIdQuery } from "../api/getInvoices";
 
+import {
+  Page,
+  Image,
+  Text,
+  View,
+  Document,
+  StyleSheet,
+  PDFViewer,
+} from "@react-pdf/renderer";
+import logo from "../images/columbia_logo_white.png";
 
-// Create styles
 const styles = StyleSheet.create({
-    page: {
-        flexDirection: 'row',
-        backgroundColor: '#E4E4E4'
-    },
-    section: {
-        margin: 10,
-        padding: 10,
-        flexGrow: 1
-    }
+  page: {
+    flexDirection: "row",
+    backgroundColor: "#E4E4E4",
+  },
+  section: {
+    margin: 10,
+    padding: 10,
+    flexGrow: 1,
+  },
 });
 
-
-const invoicePDF = () => {
-    const { id } = useParams();
-    console.log("id: ", id);
-    console.log("what in the holly hell")
-    return (
-        <Document>
-            <Page size="A4" style={styles.page}>
-                <View style={styles.section}>
-                    <Text>Viewing Invoie: {id}</Text>
-                </View>
-                <View style={styles.section}>
-                    <Text>Section #2</Text>
-                </View>
-            </Page>
-        </Document>
-    );
-
-};
-
-
 const InvoiceView = () => {
-    const { id } = useParams();
-    const { data, error, isLoading } = useGetInvoiceByIdQuery(id);
-    // console.log("invoice data: ", data);
-    if (isLoading) {
-        return <div>Loading...</div>
-    } else if (error) {
-        return <div>Error: {error.message}</div>
-    } else if (data) {
-        const { invoiceNumber, companyName, products, description, createdAt } = data;
-        return (
+  const { id } = useParams();
+  const { data, error, isLoading } = useGetInvoiceByIdQuery(id);
+  // console.log("invoice data: ", data);
+  if (isLoading) {
+    return <div>Loading...</div>;
+  } else if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (data) {
+    const { invoiceNumber, companyName, products, description, createdAt } =
+      data;
 
-            <div>
-                your invice id is: {id}
-                <div>Invoice Number: {invoiceNumber}</div>
-                <div>Company Name: {companyName}</div>
-                {products.map((invProduct, iny) => (
-                    <div key={iny}>
-                        {console.log(invProduct)}
-                    </div>
-                ))}
-                <div className="w-full h-[750px]">
-                    <PDFViewer width="100%" height="100%">
-                        <invoicePDF />
-                    </PDFViewer>
-                </div>
-            </div>
-
-        );
-    }
-
-
-
-
+    return (
+      <div>
+        <div className="w-full h-[750px]">
+          <PDFViewer style={{ width: "100vw", height: "100vh" }}>
+            <Document title="Invoice# goes here">
+              <Page size="A4" style={styles.page}>
+                <View locale="en-US">
+                  <Image
+                    src={logo}
+                    style={{ marginLeft: "50px", width: "500", height: "100" }}
+                  />
+                  <View style={styles.section}>
+                    <Text>Viewing Invoice:{invoiceNumber}</Text>
+                  </View>
+                  Section #2{" "}
+                  {products.map((item, index) => {
+                    return (
+                      <View key={index} style={styles.section}>
+                        <Text>Product Name: {item.itemName}</Text>
+                        <Text>Product Price: {item.itemPrice}</Text>
+                        <Text>Product Quantity: {item.quantity}</Text>
+                      </View>
+                    );
+                  })}
+                </View>
+              </Page>
+            </Document>
+          </PDFViewer>
+        </div>
+      </div>
+    );
+  }
 };
 
 export default InvoiceView;
+
+// const InvoiceView = () => {
+//     const { id } = useParams();
+//     const { data, error, isLoading } = useGetInvoiceByIdQuery(id);
+//     // console.log("invoice data: ", data);
+//     if (isLoading) {
+//         return <div>Loading...</div>
+//     } else if (error) {
+//         return <div>Error: {error.message}</div>
+//     } else if (data) {
+//         const { invoiceNumber, companyName, products, description, createdAt } = data;
+//         return (
+
+//             <div>
+//                 your invice id is: {id}
+//                 <div>Invoice Number: {invoiceNumber}</div>
+//                 <div>Company Name: {companyName}</div>
+//                 {products.map((invProduct, iny) => (
+//                     <div key={iny}>
+//                         {console.log(invProduct)}
+//                     </div>
+//                 ))}
+//                 <div className="w-full h-[750px]">
+//                     <PDFViewer width="100%" height="100%">
+//                         <invoicePDF />
+//                     </PDFViewer>
+//                 </div>
+//             </div>
+
+//         );
+//     }
+
+// };
--- src/components/PdfViewTest.js
+++ src/components/PdfViewTest.js
@@ -1,54 +1,55 @@
 import React from "react";
-import { Page, Image, Text, View, Document, StyleSheet, PDFViewer } from '@react-pdf/renderer';
-import logo from '../images/columbia_logo_white.png';
-
-
+import {
+  Page,
+  Image,
+  Text,
+  View,
+  Document,
+  StyleSheet,
+  PDFViewer,
+} from "@react-pdf/renderer";
+import logo from "../images/columbia_logo_white.png";
 
 const styles = StyleSheet.create({
-    page: {
-        flexDirection: 'row',
-        backgroundColor: '#E4E4E4'
-    },
-    section: {
-        margin: 10,
-        padding: 10,
-        flexGrow: 1
-    }
+  page: {
+    flexDirection: "row",
+    backgroundColor: "#E4E4E4",
+  },
+  section: {
+    margin: 10,
+    padding: 10,
+    flexGrow: 1,
+  },
 });
 
-
-
 const PdfViewTest = () => {
-
-    return (
-        <div>
-            <div className="w-full h-[750px]">
-                <PDFViewer style={{ width: '100vw', height: '100vh' }}>
-                    <Document title="Invoice# goes here">
-                        <Page size="A4" style={styles.page}>
-                            <View>
-                                <Image src={logo} style={{ marginLeft: '50px', width: '500', height: '100' }} />
-                                <View style={styles.section}>
-                                    <Text>Viewing Invoie:12345689</Text>
-                                </View>
-                                <View style={styles.section}>
-                                    <Text>Section #2</Text>
-                                </View>
-                            </View>
-
-
-                        </Page>
-                    </Document>
-                </PDFViewer>
-            </div>
-
-        </div>
-    )
-}
+  return (
+    <div>
+      <div className="w-full h-[750px]">
+        <PDFViewer style={{ width: "100vw", height: "100vh" }}>
+          <Document title="Invoice# goes here">
+            <Page size="A4" style={styles.page}>
+              <View locale="en-US">
+                <Image
+                  src={logo}
+                  style={{ marginLeft: "50px", width: "500", height: "100" }}
+                />
+                <View style={styles.section}>
+                  <Text>Viewing Invoie:12345689</Text>
+                </View>
+                <View style={styles.section}>
+                  <Text>Section #2</Text>
+                </View>
+              </View>
+            </Page>
+          </Document>
+        </PDFViewer>
+      </div>
+    </div>
+  );
+};
 
 export default PdfViewTest;
-
-
 
 //TEST WHEN CREATING MULTIPLE PAGES //////
 // const [numPages, setNumPages] = useState(null);
@@ -64,10 +65,8 @@ export default PdfViewTest;
 //         .map(page => <Page pageNumber={page} />)}
 // </Document>
 
-
-
-
-{/* <Document title="Invoice# goes here">
+{
+  /* <Document title="Invoice# goes here">
     <Page size="A4" style={styles.page}>
         <View style={styles.section}>
             <Text>Viewing Invoie:12345689</Text>
@@ -76,4 +75,5 @@ export default PdfViewTest;
             <Text>Section #2</Text>
         </View>
     </Page>
-</Document> */}
+</Document> */
+}

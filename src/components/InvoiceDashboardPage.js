35823b3f833e19f334ee00458e5156609bd7eaa7
Chuck Nelson <chachimcdev@gmail.com>
2025-02-15 10:05:46 -0800

    tighten up dashboard loader component

--- src/components/InvoiceDashboardPage.js
+++ src/components/InvoiceDashboardPage.js
@@ -1,12 +1,9 @@
 import React from "react";
 import InvoiceList from "./InvoiceList";
-import { useSelector } from 'react-redux';
 import InvoiceListFilters from "./InvoiceListFilters";
 
 
 const InvoiceDashboardPage = () => {
-  const filters = useSelector((state) => state.filters);
-  console.log("filters: ", filters);
   return (
     <div>
       <InvoiceListFilters />

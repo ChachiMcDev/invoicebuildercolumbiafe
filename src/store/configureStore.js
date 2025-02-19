13a81d6e2c4f4df66faf933e2c26503069336979
chuck at work <charlesnelson03@gmail.com>
2025-02-19 15:23:59 -0800

    removed excess store code

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,44 +1,25 @@
-import { configureStore, combineReducers, applyMiddleware, compose } from '@reduxjs/toolkit';
-// import expensesSlice from '../slicereducers/expensesSlice';
-// import filtersSlice from '../slicereducers/filterSlice';
-// import authSlice from '../slicereducers/authSlice';
-import { thunk } from 'redux-thunk';
-import { setupListeners } from '@reduxjs/toolkit/query'
-import { getInvoicesApi } from '../api/getInvoices';
-import filtersSlice from '../slicereducers/filterSlice';
-
-
-// import { expenseUserApi } from '../api/expenseUserApi';
-// const middlewareEnhancer = applyMiddleware(thunk);
-// const composedEnhancers = compose(middlewareEnhancer)
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { thunk } from "redux-thunk";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { getInvoicesApi } from "../api/getInvoices";
+import filtersSlice from "../slicereducers/filterSlice";
 
 export default () => {
+  const rootReducer = combineReducers({
+    [getInvoicesApi.reducerPath]: getInvoicesApi.reducer,
+    filters: filtersSlice.reducer,
+  });
 
-    const rootReducer = combineReducers({
-        [getInvoicesApi.reducerPath]: getInvoicesApi.reducer,
-        filters: filtersSlice.reducer,
-    });
+  const store = configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({ serializableCheck: false }).concat([
+        thunk,
+        getInvoicesApi.middleware,
+      ]),
+  });
 
-    const store = configureStore({
-        reducer: rootReducer,
-        middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false })
-            .concat([thunk,
-                getInvoicesApi.middleware
-            ]),
-    });
+  setupListeners(store.dispatch);
 
-    // const store = configureStore({
-    //     reducer: rootReducer,
-    //     middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false })
-    //         .concat([thunk,
-    //             getAllExpensesApi.middleware,
-    //             expenseUserApi.middleware
-    //         ]),
-    // });
-
-    setupListeners(store.dispatch)
-
-    return store;
-
+  return store;
 };
-
